7472fdbafcbebf5906fefbfe8963afdcb5e092fd
JR Minnaar <jr.minnaar@gmail.com>
2014-08-30 14:25:31 +0200

    Merge and move demo app to new subfolder

--- bin/uninstall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-unlink /etc/nginx/sites-enabled/nginx-uwsgi_djskel.conf
-unlink /etc/uwsgi/vassals/uwsgi_djskel.ini
--- src/restful_api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class CustomConfig(AppConfig):
-    name = 'restful_api'
-    verbose_name = 'REST API Demo'
