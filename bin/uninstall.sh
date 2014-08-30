4a0a9b93d9dc1b10ae9216a50722f881f41078b5
JR Minnaar <jr.minnaar@gmail.com>
2014-08-30 14:15:20 +0200

    Moved sample project to subdirectory

--- bin/uninstall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo unlink /etc/nginx/sites-enabled/nginx-uwsgi_djskel.conf
-sudo unlink /etc/uwsgi/uwsgi_djskel.ini
--- src/munin/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MuninConfig(AppConfig):
-    name = 'munin'
-    verbose_name = 'Portal Health'
